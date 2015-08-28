574160c2eab0221970a2c8a712dfb0ad2375b36d
LorDClockaN <davor@losinj.com>
2015-08-27 23:52:56 -0700

    Settings: Hide carrier on lock screen (2/2) PS2: save CarrierLabel option and add systemUI restart PS3: hide switch for WiFi only devices PS4: really hide PS5: publish edit PS6: fix whitespace PS7: fix typo in commit message PS8: fix title and remove summary PS9: add back summarry about restarting SystemUI PS10: restart UI on every switch toggle
    Change-Id: I125d38d6120316fecc3be5af44aeb693e997e44d

--- src/com/android/settings/slim/LockscreenSettings.java
+++ src/com/android/settings/slim/LockscreenSettings.java
@@ -15,26 +15,56 @@
  */
 package com.android.settings.slim;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.CheckBoxPreference;
+import android.preference.SwitchPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.slimremix.util.AbstractAsyncSuCMDProcessor;
+import com.android.settings.slimremix.util.CMDProcessor;
+import com.android.settings.slimremix.util.Helpers;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class LockscreenSettings extends SettingsPreferenceFragment
         implements OnPreferenceChangeListener {
 
     private static final String LOCKSCREEN_BOTTOM_SHORTCUTS = "lockscreen_bottom_shortcuts";
+    private static final String CARRIERLABEL_ON_LOCKSCREEN="lock_screen_hide_carrier";
 
+    private SwitchPreference mCarrierLabelOnLockScreen;
     private SwitchPreference mLockscreenBottomShortcuts;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.lockscreen_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefSet = getPreferenceScreen();
 
         // Lockscreen bottom shortcuts
         mLockscreenBottomShortcuts = (SwitchPreference) findPreference(LOCKSCREEN_BOTTOM_SHORTCUTS);
@@ -47,15 +77,40 @@ public class LockscreenSettings extends SettingsPreferenceFragment
                       R.string.lockscreen_bottom_shortcuts_disabled);
             mLockscreenBottomShortcuts.setOnPreferenceChangeListener(this);
         }
+
+        //CarrierLabel on LockScreen
+        mCarrierLabelOnLockScreen = (SwitchPreference) findPreference(CARRIERLABEL_ON_LOCKSCREEN);
+        if (!Utils.isWifiOnly(getActivity())) {
+            mCarrierLabelOnLockScreen.setOnPreferenceChangeListener(this);
+
+            boolean hideCarrierLabelOnLS = Settings.System.getInt(
+                    getActivity().getContentResolver(),
+                    Settings.System.LOCK_SCREEN_HIDE_CARRIER, 0) == 1;
+            mCarrierLabelOnLockScreen.setChecked(hideCarrierLabelOnLS);
+        } else {
+            prefSet.removePreference(mCarrierLabelOnLockScreen);
+        }
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
         if (preference == mLockscreenBottomShortcuts) {
             Settings.Secure.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.Secure.LOCKSCREEN_BOTTOM_SHORTCUTS,
-                    (Boolean) value ? 1 : 0);
+                    (Boolean) objValue ? 1 : 0);
+            return true;
+        } else if (preference == mCarrierLabelOnLockScreen) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCK_SCREEN_HIDE_CARRIER,
+                    (Boolean) objValue ? 1 : 0);
+            Helpers.restartSystemUI();
+            return true;
         }
-        return true;
+        return false;
+    }
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
+ 		return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }
