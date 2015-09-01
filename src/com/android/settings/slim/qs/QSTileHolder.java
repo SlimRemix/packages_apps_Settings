3877e2644a0379996ec47332aeb6f46e58b0a28a
stofstik <verstegen.daan@gmail.com>
2015-09-01 01:14:33 -0700

    Added show volume to tile chooser
    Change-Id: I60e521096852669ffe4d379b24d6a25de0a2d6f4
    JIRA: CYAN-6721
    
    Conflicts:
    	res/values/slim_strings.xml

--- src/com/android/settings/slim/qs/QSTileHolder.java
+++ src/com/android/settings/slim/qs/QSTileHolder.java
@@ -169,6 +169,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_ringer_audible";
                 stringId = R.string.qs_tile_notifications;
                 break;
+            case QSConstants.TILE_VOLUME:
+                resourceName = "ic_qs_volume_panel";
+                stringId = R.string.qs_tile_volume_panel;
+                break;
             default:
                 return null;
         }
